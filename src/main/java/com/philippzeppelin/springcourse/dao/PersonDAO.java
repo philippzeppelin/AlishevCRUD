259b324e1e7da08ecc30c05faa06adbb69c6fc22
Philipp Zeppelin <philippzeppelin>
2024-04-02 22:31:25 +0300

    Finished changing hibernate project tp hibernate + spring

--- src/main/java/com/philippzeppelin/springcourse/dao/PersonDAO.java
+++ src/main/java/com/philippzeppelin/springcourse/dao/PersonDAO.java
@@ -1,50 +1,98 @@
 package com.philippzeppelin.springcourse.dao;
 
 import com.philippzeppelin.springcourse.models.Person;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private final JdbcTemplate jdbcTemplate;
+    private final SessionFactory sessionFactory;
 
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
+    @Transactional(readOnly = true)
     public List<Person> index() {
-        return jdbcTemplate.query("SELECT * FROM Person;",
-                new BeanPropertyRowMapper<>(Person.class));
+        Session session = sessionFactory.getCurrentSession();
+
+        // здесь будет наш обычный hibernate код
+
+        return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?",
-                        new Object[]{id},
-                        new BeanPropertyRowMapper<>(Person.class)).stream()
-                .findAny()
-                .orElse(null);
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
     }
 
+    @Transactional
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
-                person.getName(),
-                person.getAge(),
-                person.getEmail()
-        );
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional
     public void update(int id, Person updatedPerson) {
-        jdbcTemplate.update("UPDATE Person SET name = ?, age = ? WHERE email = ?",
-                updatedPerson.getName(),
-                updatedPerson.getAge(),
-                updatedPerson.getEmail()
-        );
+        Session session = sessionFactory.getCurrentSession();
+        Person personToBeUpdated = session.get(Person.class, id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
+    @Transactional
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Person.class, id));
     }
 }
+
+//    JdbcTemplate \/
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public PersonDAO(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//
+//    public List<Person> index() {
+//        return jdbcTemplate.query("SELECT * FROM Person;",
+//                new BeanPropertyRowMapper<>(Person.class));
+//    }
+//
+//    public Person show(int id) {
+//        return jdbcTemplate.query("SELECT * FROM Person WHERE id = ?",
+//                        new Object[]{id},
+//                        new BeanPropertyRowMapper<>(Person.class)).stream()
+//                .findAny()
+//                .orElse(null);
+//    }
+//
+//    public void save(Person person) {
+//        jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?, ?)",
+//                person.getName(),
+//                person.getAge(),
+//                person.getEmail()
+//        );
+//    }
+//
+//    public void update(int id, Person updatedPerson) {
+//        jdbcTemplate.update("UPDATE Person SET name = ?, age = ? WHERE email = ?",
+//                updatedPerson.getName(),
+//                updatedPerson.getAge(),
+//                updatedPerson.getEmail()
+//        );
+//    }
+//
+//    public void delete(int id) {
+//        jdbcTemplate.update("DELETE FROM Person WHERE id = ?", id);
+//    }
+//}
--- src/main/java/com/philippzeppelin/springcourse/models/Person.java
+++ src/main/java/com/philippzeppelin/springcourse/models/Person.java
@@ -1,27 +1,38 @@
 package com.philippzeppelin.springcourse.models;
 
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "Person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
+
     @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
     private int age;
+
     @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
+    @Email
+    @Column(name = "email")
     private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
+    public Person(String name, int age, String email) {
         this.name = name;
         this.age = age;
         this.email = email;
@@ -57,5 +68,15 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
